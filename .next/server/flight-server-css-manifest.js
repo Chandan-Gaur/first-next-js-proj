6bd075557120305492748e1e3f9d9995b7913a4b
Chandan Gaur <chandangaurjecrc@gmail.com>
2023-05-14 20:12:20 +0530

    5th push

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,21 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/chandangaur/development/Next JS Proj/first-next-app/app/layout.tsx": [
+      "/Users/chandangaur/development/Next JS Proj/first-next-app/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/chandangaur/development/Next JS Proj/first-next-app/app/globals.css"
+    ],
+    "/Users/chandangaur/development/Next JS Proj/first-next-app/app/page.tsx": [
+      "/Users/chandangaur/development/Next JS Proj/first-next-app/node_modules/next/font/google/target.css?{\"path\":\"app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  },
+  "cssModules": {
+    "/Users/chandangaur/development/Next JS Proj/first-next-app/app/restaurant/[slug]/page": [
+      "/Users/chandangaur/development/Next JS Proj/first-next-app/app/globals.css",
+      "/Users/chandangaur/development/Next JS Proj/first-next-app/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/Users/chandangaur/development/Next JS Proj/first-next-app/app/page": [
+      "/Users/chandangaur/development/Next JS Proj/first-next-app/app/globals.css",
+      "/Users/chandangaur/development/Next JS Proj/first-next-app/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,6 +1,3 @@
-import NaveBar from "@/app/components/NaveBar";
-import Link from "next/link";
-import Header from "./components/Header";
 import RestaurantNavBar from "./components/RestaurantNavBar";
 import Title from "./components/Title";
 import Rating from "./components/Rating";
@@ -8,19 +5,53 @@ import Description from "./components/Description";
 import Images from "./components/Images";
 import Reviews from "./components/Reviews";
 import ReservationCard from "./components/ReservationCard";
+import { PrismaClient } from "@prisma/client";
 
-export default function RestaurantDetail() {
+const prisma = new PrismaClient();
+
+interface Restaurant {
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+}
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    },
+  });
+  if (!restaurant) {
+    throw Error("restaurent not defined");
+  }
+  return restaurant;
+};
+
+export default async function RestaurantDetail({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  //  console.log({ params });
+
+  const restaurant = await fetchRestaurantBySlug(params.slug);
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
         <RestaurantNavBar />
         <Title />
         <Rating />
-
         <Description />
-
         <Images />
-
         <Reviews />
       </div>
       <div className="w-[27%] relative text-reg">
